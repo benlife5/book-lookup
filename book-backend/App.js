50ff79f39396ac33fe1aff7924acb575d7855428
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-05-26 15:59:18 -0400

    added basic API functionality

--- book-backend/App.js
+++ book-backend/App.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const bookData = require("./books_simple.json");
 const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!!!");
+});
+
+app.get("/book", (req, res) => {
+  if (req.query.title) {
+    const title = req.query.title;
+    res.json({ title: title, author: bookData[title] });
+  } else {
+    res.send("Book Lookup API: please pass arguments");
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Book-Lookup app listening at http://localhost:${port}`);
 });
